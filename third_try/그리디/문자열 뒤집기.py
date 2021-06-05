6e940533b7352a4920388a8ef74e0c00a72b66b1
Err0rCode7 <aa6184708@naver.com>
2021-06-05 17:19:23 +0900

    # third_try: 문자열뒤집기

---
+++ third_try/그리디/문자열 뒤집기.py
@@ -0,0 +1,26 @@
+# BOJ 1439
+
+# test case
+
+# Q:
+# 0001100
+# A:
+# 1
+
+import sys
+
+input = sys.stdin.readline
+
+string = input().rstrip()
+
+target = string[0]
+result = 0
+
+for i in range(1, len(string)):
+	if target != string[i] and string[i] != string[i - 1]:
+		result += 1
+
+if result == 0:
+	result += 1
+
+print(result)
