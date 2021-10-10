7f4cdd545715bb0ee525e9a6294598ffdf1623c6
Err0rCode7 <aa6184708@naver.com>
2021-10-10 19:59:03 +0900

    Modify Typo

--- baekjoon/dijkstra_graph/dijkstra.py
+++ baekjoon/dijkstra_graph/dijkstra.py
@@ -20,13 +20,13 @@ distance = [INF] * (n + 1)
 
 for _ in range(m) :
 	node, to_node, value = map(int, sys.stdin.readline().rstrip().split())
-	graph[obj_node].append((to_node, value))
+	graph[node].append((to_node, value))
 
 def get_smallest_node() :
 	min_value = INF
 	index = 0
 	for i in range(1, n + 1) :
-		if (distance[i] < min_value and not visited[i])
+		if (distance[i] < min_value and not visited[i]):
 			min_value = distance[i]
 			index = i
 	return index
