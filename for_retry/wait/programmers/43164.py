80b27d5dc92220fb319f66571051c2fa2b50cec8
Err0rCode7 <aa6184708@naver.com>
2021-12-11 01:13:24 +0900

    20211211

--- for_retry/wait/programmers/43164.py
+++ for_retry/wait/programmers/43164.py
@@ -1,35 +1,28 @@
 import heapq
-
-def dfs(tickets, visited, move_list, result, n) :
-
-	if len(move_list) == n + 1:
-		# print(move_list)
-		heapq.heappush(result, list(move_list))
-		return
-
-	for i in range(n):
-		start, end = tickets[i]
-		# print("start", start, "end", end)
-		if visited[i] == True or move_list[-1] != start:
-			continue
-		# print("from:", move_list[-1], "to", start, "to", end)
-		visited[i] = True
-		move_list.append(end)
-		dfs(tickets, visited, move_list, result, n)
-		visited[i] = False
-		move_list.pop()
-
+from collections import defaultdict, deque
 
 def solution(tickets):
 
-	n = len(tickets)
-	visited = [False] * n
+	graph = defaultdict(list)
 
-	move_list = ["ICN"]
-	result = []
-	dfs(tickets, visited, move_list, result, n)
+	def init_graph(graph, tickets):
+		for start, end in tickets:
+			heapq.heappush(graph[start], end)
 	
-	return result[0]
+	init_graph(graph, tickets)
+
+	stack = deque()
+	stack.append("ICN")
+	result = []
+	n = len(tickets)
+	while stack :
+		start = stack[-1]
+		if graph[start] :
+			stack.append(heapq.heappop(graph[start]))
+		else :
+			result.append(stack.pop())
+	result.reverse()
+	return (result)
 
 print(solution([["ICN", "JFK"], ["HND", "IAD"], ["JFK", "HND"]]	))
 print(solution([["ICN", "SFO"], ["ICN", "ATL"], ["SFO", "ATL"], ["ATL", "ICN"], ["ATL","SFO"]]	))
